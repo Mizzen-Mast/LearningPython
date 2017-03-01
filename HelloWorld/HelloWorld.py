b4a834341d16e93c9e7da388e8df4717e1802fe0
kyle-clayson <kyle.clayson@gmail.com>
2017-03-01 14:26:59 -0700

    Added Hello World

---
+++ HelloWorld/HelloWorld.py
@@ -0,0 +1,10 @@
+#print out hello world
+print "Hello World" 
+
+#store Hello World in a variable
+phrase = "Hello World"
+print(phrase)
+
+#store "Hello World" in a variable
+phrase = "\"Hello World\""
+print(phrase)
