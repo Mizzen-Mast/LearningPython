a0b44d6c1a68ebcc8dc69d15a693328717474f5a
Kyle <kyle.clayson@gmail.com>
2017-03-16 16:04:23 -0600

    Completed Challege for 3 Easy

--- Challenges/Three/threeEasyExtra.py
+++ Challenges/Three/threeEasyExtra.py
@@ -4,6 +4,7 @@ from sys import argv, exit
 
 types = {
  'encrypt': lambda phrase : codecs.encode(str(phrase), 'rot_13'),
+ 'decrypt': lambda phrase : codecs.decode(str(phrase), 'rot_13') 
 } 
 if (argv[1]) not in types:
   print "Error: Wrong type: " ((argv[1]), '|'.join(types))
